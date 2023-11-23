160b4c3a858d2a76815b6be70fddab6b8f6f44b2
NirZabari <nirzabari@gmail.com>
2023-11-24 01:48:50 +0200

    thumbnails check

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,5 @@
 for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 300x900 "tn/$file"
+#    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 300x900 "tn/$file"
+    [ ! -f "tn/$file" ] && convert "$file"  -resize 50%  "tn/$file"
+
 done
